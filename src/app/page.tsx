8cdc37ed778587c8d25b576e729b383210a365ff
Hardik Kheni <hardikkheni.work@gmail.com>
2024-11-13 11:58:44 +0530

    storybook setup.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { ModeToggle } from '@/components/mode-toggle';
-
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <ModeToggle />
+      Welcome to Next.js!
     </div>
   );
 }
