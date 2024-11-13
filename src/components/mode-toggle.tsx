9ca803daac377ee80f876dec126cae5d8e34a665
Hardik Kheni <hardikkheni.work@gmail.com>
2024-11-13 11:26:50 +0530

    init commit with shadcn setup.

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import * as React from 'react';
+import { useTheme } from 'next-themes';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+export function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="px-2 py-1">
+          <span className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0">Sun</span>
+          <span className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100">Moon</span>
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
